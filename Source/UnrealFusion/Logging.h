4d8c651785f7e163bc054bda998b7c748b571b85
Jake Fountain <jake.fountain@gmail.com>
2017-03-02 15:15:40 +1100

    Added copyright info

--- Source/UnrealFusion/Logging.h
+++ Source/UnrealFusion/Logging.h
@@ -1,7 +1,23 @@
+/*  This file is part of UnrealFusion, a sensor fusion plugin for VR in the Unreal Engine
+    Copyright (C) 2017 Jake Fountain
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "UnrealFusion.h"
 #include <string>
 #pragma once
 
+//Unreal Engine specific log method. Would need replacement for use in other systems
 inline void FUSION_LOG(std::string s){
 	FString str(s.c_str());
 	UE_LOG(LogTemp, Warning, TEXT("FUSION LOG : %s"),*str);
