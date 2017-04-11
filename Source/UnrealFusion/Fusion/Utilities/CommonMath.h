20cdbebd9150262bcf8292e4466dc81207e7b59a
Jake Fountain <jake.fountain@gmail.com>
2017-04-11 17:53:44 +1000

    added some commented debug code for later. verified that the latency method seems to work reasonably well

--- Source/UnrealFusion/Fusion/Utilities/CommonMath.h
+++ Source/UnrealFusion/Fusion/Utilities/CommonMath.h
@@ -101,6 +101,7 @@ namespace fusion{
 					max_count = histogram[i];
 					max_index = i;
 				}
+				//FUSION_LOG("Histogram: " + std::to_string(i * delta + min) + " count = " + std::to_string(histogram[i]));
 			}
 
 			//Return centred max bin
