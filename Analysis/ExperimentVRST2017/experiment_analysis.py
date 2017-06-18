be1be13ef7ec9659bf9c0e70548aca96ee8c08a5
Jake Fountain <jake.fountain@gmail.com>
2017-06-18 10:03:47 +1000

    recorded data for literature method. very similar means. set up correct rendering from kinect pov

--- Analysis/ExperimentVRST2017/experiment_analysis.py
+++ Analysis/ExperimentVRST2017/experiment_analysis.py
@@ -7,7 +7,7 @@ import math
 from numpy import genfromtxt
 from covariance_plot import plot_point_cov
 
-def plotData(folderName, N):
+def plotData(folderName, N, point_spec, ellipse_color):
 	errors = np.zeros([N,2])
 	for i in range(N):
 		my_data = genfromtxt(folderName + '/' + str(i+1), delimiter=',')
@@ -20,12 +20,38 @@ def plotData(folderName, N):
 	print "Worst Individual= ", errors.max(axis=0)
 	print "Best Individual= ", errors.min(axis=0)
 	
-	plot_point_cov(errors,color='grey')
-	plt.plot(errors[:,0], errors[:,1], '.k')
+	plot_point_cov(errors,color=ellipse_color)
+	plt.plot(errors[:,0], errors[:,1], point_spec)
 	plt.axis([0,30,0,8])
 	plt.plot(mean[0],mean[1], 'rx')
-	plt.show()
+
+def plotDataUnified(folderName, N, point_spec, ellipse_color):
+	errors = np.zeros([0,2])
+	for i in range(N):
+		my_data = genfromtxt(folderName + '/' + str(i+1), delimiter=',')
+		start = 0
+		last_value = my_data[0,0]
+		for j in range(my_data.shape[0]):
+			change = np.linalg.norm(my_data[j,0] - last_value) 
+			if(change > 0.2 or j==my_data.shape[0]-1):
+				errors = np.append(errors, [my_data[start:j,:].mean(axis=0)], axis=0)
+				start = j + 1
+				last_value = my_data[j,0]
+
+	mean = errors.mean(axis=0)
+	print errors
+	print "Mean = ", errors.mean(axis=0)
+	print "StdDev = ", errors.std(axis=0)
+	print "Worst Individual= ", errors.max(axis=0)
+	print "Best Individual= ", errors.min(axis=0)
+	
+	plot_point_cov(errors,color=ellipse_color)
+	plt.plot(errors[:,0], errors[:,1], point_spec)
+	plt.axis([0,30,0,8])
+	plt.plot(mean[0],mean[1], 'rx')
 
 
-plotData('CustomMovementVive1',10)
+plotData('CustomMovementVive1',10,point_spec = 'g.', ellipse_color='blue')
+plotDataUnified('CustomMovementViveLitMethod1',1, point_spec = 'k.', ellipse_color='grey')
+plt.show()
 
